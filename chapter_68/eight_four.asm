82d74f556817d4b246459119fde7b96a207d376a
armando <mandoblue9@gmail.com>
2022-12-13 10:53:36 -0800

    going

--- chapter_68/eight_four.asm
+++ chapter_68/eight_four.asm
@@ -1,12 +1,12 @@
-.peek
-LD R0, 0(R6) ; load first element on stack into R0
-BRnzp UNDERFLOW ; check for underflow
-LD R5, 0 ; set R5 to 0 to indicate no underflow
-RTI ; return from function
+  ; Check if value in x4000 is a valid ASCII code
+    LD R0, x4000 ; load value from x4000 into R0
+    AND R0, R0, #0x7F ; mask off the top bit to check if it's a valid ASCII code
+    BRz skip ; if the value is 0, skip the print
+    ; Print the character
+    LD R1, ASCII ; load the ASCII character table into R1
+    ADD R1, R1, R0 ; add the ASCII code to the table address to get the character
+    LD R0, (R1) ; load the character from the table
+    TRAP x22 ; print the character
+    skip, HALT ; halt the program
 
-UNDERFLOW
-LD R5, 1 ; set R5 to 1 to indicate underflow
-RTI ; return from function
-
-
-;next
+ASCII, .FILL xFF00 ; ASCII character table
