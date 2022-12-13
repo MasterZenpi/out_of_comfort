edcddd5c4ba05acd5bf024176bf5e69fb21f8228
armando <mandoblue9@gmail.com>
2022-12-13 01:51:26 -0800

    new

--- chapter_68/eight_four.asm
+++ chapter_68/eight_four.asm
@@ -1,18 +1,12 @@
-PEEK
-    LD R0, STACK_POINTER    ; Load the stack pointer into R0
-    ADD R0, R0, #-1         ; Decrement the stack pointer to point to the top of the stack
-    LD R1, STACK(R0)        ; Load the value at the top of the stack into R1
-    BRnzp PEEK_UNDERFLOW    ; If the stack pointer is 0, go to the underflow error handling routine
-    LD R0, STACK_POINTER    ; Load the stack pointer back into R0
-    ADD R0, R0, #1          ; Increment the stack pointer to restore its original value
-    RET                     ; Return the value in R1
+.peek
+LD R0, 0(R6) ; load first element on stack into R0
+BRnzp UNDERFLOW ; check for underflow
+LD R5, 0 ; set R5 to 0 to indicate no underflow
+RTI ; return from function
 
-PEEK_UNDERFLOW
-    ; Handle the underflow error by printing an error message and halting the program
-    LEA R0, STACK_UNDERFLOW_MESSAGE
-    PUTS
-    HALT
+UNDERFLOW
+LD R5, 1 ; set R5 to 1 to indicate underflow
+RTI ; return from function
 
-; Define the STACK_POINTER and STACK symbols for use in the PEEK function
-STACK_POINTER .FILL #0
-STACK .FILL x3000
+
+;next
