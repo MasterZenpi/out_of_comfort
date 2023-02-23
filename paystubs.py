33d669951045b65479024ca38f844df194e91275
MasterZenpi <52257282+MasterZenpi@users.noreply.github.com>
2023-02-23 00:15:12 -0800

    Update paystubs.py

--- paystubs.py
+++ paystubs.py
@@ -1,15 +1,10 @@
+import tabula
 from pdfminer.high_level import extract_text
-text = extract_text('Earnings.pdf')
-
-#getts the pay period dates
-date = text.split('\n')[47] + text.split('\n')[48]
-
-
 import os
 import PyPDF2
 
 # specify the folder path
-folder_path = "path/to/folder"
+folder_path = "sierra_hours"
 
 # list all files in the folder
 files = os.listdir(folder_path)
@@ -17,15 +12,17 @@ files = os.listdir(folder_path)
 # filter for only the PDF files
 pdf_files = [f for f in files if f.endswith('.pdf')]
 
-# loop through each PDF file and open it
-for file in pdf_files:
-    # create a PDF reader object
-    pdf_reader = PyPDF2.PdfFileReader(open(os.path.join(folder_path, file), 'rb'))
+########################################################################################
 
-    # loop through each page of the PDF file
-    for page_num in range(pdf_reader.numPages):
-        # extract the text from the page
-        page_text = pdf_reader.getPage(page_num).extractText()
+tables = tabula.read_pdf("Earnings.pdf", pages='all')
+text = extract_text('Earnings.pdf')
+df = tables[0]
 
-        # do something with the page text (e.g. print it)
-        print(page_text)
+#hours worked in one pay period  
+hours = df.iloc[1][1]
+
+#getts the pay period dates
+date = text.split('\n')[47] + text.split('\n')[48]
+new_row = {date:hours}
+
+########################################################################################
