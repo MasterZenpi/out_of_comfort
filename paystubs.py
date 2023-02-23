c528856f8b481677c3fb0eabdca8eb082521467d
MasterZenpi <52257282+MasterZenpi@users.noreply.github.com>
2023-02-23 00:22:23 -0800

    Update paystubs.py

--- paystubs.py
+++ paystubs.py
@@ -1,7 +1,7 @@
 import tabula
 from pdfminer.high_level import extract_text
 import os
-import PyPDF2
+import pdfplumber
 
 # specify the folder path
 folder_path = "sierra_hours"
@@ -12,17 +12,16 @@ files = os.listdir(folder_path)
 # filter for only the PDF files
 pdf_files = [f for f in files if f.endswith('.pdf')]
 
-########################################################################################
+# loop through each PDF file and open it
+for file in pdf_files:
+    # create a PDF object
+  tables = tabula.read_pdf(file, pages='all')
+  text = extract_text(file)
+  df = tables[0]
 
-tables = tabula.read_pdf("Earnings.pdf", pages='all')
-text = extract_text('Earnings.pdf')
-df = tables[0]
-
-#hours worked in one pay period  
-hours = df.iloc[1][1]
-
-#getts the pay period dates
-date = text.split('\n')[47] + text.split('\n')[48]
-new_row = {date:hours}
-
-########################################################################################
+  #hours worked in one pay period  
+  hours = df.iloc[1][1]
+  
+  #getts the pay period dates
+  date = text.split('\n')[47] + text.split('\n')[48]
+  new_row = {date:hours}
